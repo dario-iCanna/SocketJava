cf58c5a3e8663485f607ff405b39bd5f527808dd
lai2-24 <lai2-24@itismeucci.com>
2024-10-03 13:32:18 +0200

    Finito

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
@@ -13,7 +14,9 @@ public class Main {
         Socket s = new Socket("localhost",3000);
         BufferedReader in = new BufferedReader(new InputStreamReader((s.getInputStream())));
         DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        out.writeBytes("ciao" + '\n');
+        Scanner input = new Scanner(System.in);
+        System.out.println("inserisci stringa");
+        out.writeBytes(""+ input.next() + '\n');
         String stringaRicevuta = in.readLine();
         System.out.println("la string ricevuta e' " + stringaRicevuta);
         s.close();
